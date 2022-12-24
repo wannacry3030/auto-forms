2d8ec256495f1392024356c4cf2eed977378f979
wannacry3030 <wannacry.bat@proton.me>
2022-12-24 19:01:00 -0300

    modified:   A.I_code.py

--- A.I_code.py
+++ A.I_code.py
@@ -1,4 +1,4 @@
-from selenium.webdriver.common.action_chains import ActionChains
+import time
 from selenium import webdriver
 from selenium.webdriver import Chrome
 from selenium.webdriver.chrome.service import Service
@@ -10,29 +10,94 @@ options.add_experimental_option("detach", True)
 servico = Service(ChromeDriverManager().install())
 
 navegador = webdriver.Chrome(options=options, service=servico)
-# Create a new ActionChains object
-actions = ActionChains(navegador)
+
 
 #passo1
 navegador.get("https://forms.office.com/Pages/ResponsePage.aspx?id=GUvwznZ3lEq4mzdcd6j5NqOa5rHBDstAhO363trnMtJUNlJKNFVEOVlUTUlTNkZLR0MzQjNDQlVROCQlQCN0PWcu&wdLOR=c3AADC4C4-C6DA-4993-90EC-8BE19D05F1B6")
 
-# Add actions to the ActionChains object
-actions.click(navegador.find_element('xpath', '//*[@id="SelectId_0_placeholder"]'))
-actions.click(navegador.find_element('xpath', '//*[@id="SelectId_0"]/div[2]/div[2]'))
-actions.send_keys_to_element(navegador.find_element('xpath', '//*[@id="form-main-content"]/div/div[1]/div[2]/div[2]/div[2]/div/div[3]/div/div/input'), "181766")
-actions.send_keys_to_element(navegador.find_element('xpath', '//*[@id="form-main-content"]/div/div[1]/div[2]/div[2]/div[3]/div/div[3]/div/div/input'), "Horizonte Logistica")
-actions.send_keys_to_element(navegador.find_element('xpath', '//*[@id="form-main-content"]/div/div[1]/div[2]/div[2]/div[4]/div/div[3]/div/div/input'), "lucas.oliveira@grupohorizonte.com.br")
-actions.click(navegador.find_element('xpath', '//*[@id="SelectId_1_placeholder"]/i'))
-actions.click(navegador.find_element('xpath', '//*[@id="SelectId_1"]/div[2]/div[1]'))
-actions.click(navegador.find_element('xpath', '//*[@id="SelectId_2_placeholder"]'))
-actions.click(navegador.find_element('xpath', '//*[@id="SelectId_2"]/div[2]/div[11]'))
-actions.click(navegador.find_element('xpath', '//*[@id="form-main-content"]/div/div[1]/div[2]/div[2]/div[7]/div/div[3]/div/div[2]/div/label/input'))
-actions.click(navegador.find_element('xpath', '//*[@id="form-main-content"]/div/div[1]/div[2]/div[2]/div[8]/div/div[3]/div/div[2]/div/label/input'))
-actions.click(navegador.find_element('xpath', '//*[@id="SelectId_3_placeholder"]'))
-actions.click(navegador.find_element('xpath', '//*[@id="SelectId_3"]/div[2]/div[3]'))
-actions.click(navegador.find_element('xpath', '//*[@id="SelectId_4_placeholder"]'))
-actions.click(navegador.find_element('xpath', '//*[@id="SelectId_4"]/div[2]/div[11]'))
+time.sleep(2)
+#passo2
+navegador.find_element('xpath', '//*[@id="SelectId_0_placeholder"]').click()
+navegador.find_element('xpath', '//*[@id="SelectId_0"]/div[2]/div[2]').click()
+navegador.find_element('xpath', '//*[@id="form-main-content"]/div/div[1]/div[2]/div[2]/div[2]/div/div[3]/div/div/input').send_keys("181766")
+navegador.find_element('xpath', '//*[@id="form-main-content"]/div/div[1]/div[2]/div[2]/div[3]/div/div[3]/div/div/input').send_keys("Horizonte Logistica")
+navegador.find_element('xpath', '//*[@id="form-main-content"]/div/div[1]/div[2]/div[2]/div[4]/div/div[3]/div/div/input').send_keys("lucas.oliveira@grupohorizonte.com.br")
+navegador.find_element('xpath', '//*[@id="SelectId_1_placeholder"]/i').click()
+navegador.find_element('xpath', '//*[@id="SelectId_1"]/div[2]/div[1]').click()
+navegador.find_element('xpath', '//*[@id="SelectId_2_placeholder"]').click()
+navegador.find_element('xpath', '//*[@id="SelectId_2"]/div[2]/div[11]').click()
+navegador.find_element('xpath', '//*[@id="form-main-content"]/div/div[1]/div[2]/div[2]/div[7]/div/div[3]/div/div[2]/div/label/input').click()
+navegador.find_element('xpath', '//*[@id="form-main-content"]/div/div[1]/div[2]/div[2]/div[8]/div/div[3]/div/div[2]/div/label/input').click()
+navegador.find_element('xpath', '//*[@id="SelectId_3_placeholder"]').click()
 
-# Perform all of the actions in the ActionChains object
-actions.perform()
+navegador.find_element('xpath', '//*[@id="SelectId_3"]/div[2]/div[2]').click()
+navegador.find_element('xpath', '//*[@id="SelectId_4_placeholder"]/i').click()
+navegador.find_element('xpath', '//*[@id="SelectId_4"]/div[2]/div[11]').click()
 
+navegador.find_element('xpath', '//*[@id="form-main-content"]/div/div[1]/div[2]/div[2]/div[11]/div/div[3]/div/div/textarea').send_keys('Preenchimento do formulario')
+navegador.find_element('xpath', '//*[@id="form-main-content"]/div/div[1]/div[2]/div[2]/div[12]/div/div[3]/div/div/textarea').send_keys('Infestação de aedes aegypti (dengue), na sala ADM HORIZONTE..')
+navegador.find_element('xpath', '//*[@id="form-main-content"]/div/div[1]/div[2]/div[3]/div[1]/button/div').click()
+
+
+
+
+
+# Open a new tab
+navegador.execute_script("window.open('https://forms.office.com/Pages/ResponsePage.aspx?id=GUvwznZ3lEq4mzdcd6j5NqOa5rHBDstAhO363trnMtJUNlJKNFVEOVlUTUlTNkZLR0MzQjNDQlVROCQlQCN0PWcu&wdLOR=c3AADC4C4-C6DA-4993-90EC-8BE19D05F1B6', '_blank');")
+
+# Switch to the new tab
+navegador.switch_to.window(navegador.window_handles[1])
+
+time.sleep(2)
+#passo2
+navegador.find_element('xpath', '//*[@id="SelectId_0_placeholder"]').click()
+navegador.find_element('xpath', '//*[@id="SelectId_0"]/div[2]/div[2]').click()
+navegador.find_element('xpath', '//*[@id="form-main-content"]/div/div[1]/div[2]/div[2]/div[2]/div/div[3]/div/div/input').send_keys("181766")
+navegador.find_element('xpath', '//*[@id="form-main-content"]/div/div[1]/div[2]/div[2]/div[3]/div/div[3]/div/div/input').send_keys("Horizonte Logistica")
+navegador.find_element('xpath', '//*[@id="form-main-content"]/div/div[1]/div[2]/div[2]/div[4]/div/div[3]/div/div/input').send_keys("lucas.oliveira@grupohorizonte.com.br")
+navegador.find_element('xpath', '//*[@id="SelectId_1_placeholder"]/i').click()
+navegador.find_element('xpath', '//*[@id="SelectId_1"]/div[2]/div[1]').click()
+navegador.find_element('xpath', '//*[@id="SelectId_2_placeholder"]').click()
+navegador.find_element('xpath', '//*[@id="SelectId_2"]/div[2]/div[11]').click()
+navegador.find_element('xpath', '//*[@id="form-main-content"]/div/div[1]/div[2]/div[2]/div[7]/div/div[3]/div/div[2]/div/label/input').click()
+navegador.find_element('xpath', '//*[@id="form-main-content"]/div/div[1]/div[2]/div[2]/div[8]/div/div[3]/div/div[2]/div/label/input').click()
+navegador.find_element('xpath', '//*[@id="SelectId_3_placeholder"]').click()
+
+navegador.find_element('xpath', '//*[@id="SelectId_3"]/div[2]/div[2]').click()
+navegador.find_element('xpath', '//*[@id="SelectId_4_placeholder"]/i').click()
+navegador.find_element('xpath', '//*[@id="SelectId_4"]/div[2]/div[11]').click()
+
+navegador.find_element('xpath', '//*[@id="form-main-content"]/div/div[1]/div[2]/div[2]/div[11]/div/div[3]/div/div/textarea').send_keys('Preenchimento do formulario')
+navegador.find_element('xpath', '//*[@id="form-main-content"]/div/div[1]/div[2]/div[2]/div[12]/div/div[3]/div/div/textarea').send_keys('Infestação de aedes aegypti (dengue), na sala ADM HORIZONTE..')
+navegador.find_element('xpath', '//*[@id="form-main-content"]/div/div[1]/div[2]/div[3]/div[1]/button/div').click()
+
+
+
+# Open a new tab
+navegador.execute_script("window.open('https://forms.office.com/Pages/ResponsePage.aspx?id=GUvwznZ3lEq4mzdcd6j5NqOa5rHBDstAhO363trnMtJUNlJKNFVEOVlUTUlTNkZLR0MzQjNDQlVROCQlQCN0PWcu&wdLOR=c3AADC4C4-C6DA-4993-90EC-8BE19D05F1B6', '_blank');")
+
+# Switch to the new tab
+navegador.switch_to.window(navegador.window_handles[2])
+
+time.sleep(2)
+#passo2
+navegador.find_element('xpath', '//*[@id="SelectId_0_placeholder"]').click()
+navegador.find_element('xpath', '//*[@id="SelectId_0"]/div[2]/div[2]').click()
+navegador.find_element('xpath', '//*[@id="form-main-content"]/div/div[1]/div[2]/div[2]/div[2]/div/div[3]/div/div/input').send_keys("181766")
+navegador.find_element('xpath', '//*[@id="form-main-content"]/div/div[1]/div[2]/div[2]/div[3]/div/div[3]/div/div/input').send_keys("Horizonte Logistica")
+navegador.find_element('xpath', '//*[@id="form-main-content"]/div/div[1]/div[2]/div[2]/div[4]/div/div[3]/div/div/input').send_keys("lucas.oliveira@grupohorizonte.com.br")
+navegador.find_element('xpath', '//*[@id="SelectId_1_placeholder"]/i').click()
+navegador.find_element('xpath', '//*[@id="SelectId_1"]/div[2]/div[1]').click()
+navegador.find_element('xpath', '//*[@id="SelectId_2_placeholder"]').click()
+navegador.find_element('xpath', '//*[@id="SelectId_2"]/div[2]/div[11]').click()
+navegador.find_element('xpath', '//*[@id="form-main-content"]/div/div[1]/div[2]/div[2]/div[7]/div/div[3]/div/div[2]/div/label/input').click()
+navegador.find_element('xpath', '//*[@id="form-main-content"]/div/div[1]/div[2]/div[2]/div[8]/div/div[3]/div/div[2]/div/label/input').click()
+navegador.find_element('xpath', '//*[@id="SelectId_3_placeholder"]').click()
+
+navegador.find_element('xpath', '//*[@id="SelectId_3"]/div[2]/div[2]').click()
+navegador.find_element('xpath', '//*[@id="SelectId_4_placeholder"]/i').click()
+navegador.find_element('xpath', '//*[@id="SelectId_4"]/div[2]/div[11]').click()
+
+navegador.find_element('xpath', '//*[@id="form-main-content"]/div/div[1]/div[2]/div[2]/div[11]/div/div[3]/div/div/textarea').send_keys('Preenchimento do formulario')
+navegador.find_element('xpath', '//*[@id="form-main-content"]/div/div[1]/div[2]/div[2]/div[12]/div/div[3]/div/div/textarea').send_keys('Infestação de aedes aegypti (dengue), na sala ADM HORIZONTE..')
+navegador.find_element('xpath', '//*[@id="form-main-content"]/div/div[1]/div[2]/div[3]/div[1]/button/div').click()
