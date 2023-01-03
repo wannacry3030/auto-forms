b29f243efcf1f45f7d702a30f3dd253477d14111
wannacry3030 <wannacry.bat@proton.me>
2023-01-03 16:54:04 -0300

    new file:   ifollow.py

---
+++ ifollow.py
@@ -0,0 +1,6 @@
+import time
+from selenium import webdriver
+from selenium.webdriver import Chrome
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+
